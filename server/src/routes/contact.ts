8a2abc6c5cba18216c54884d9480190dc6e54cba
wyatth7 <whardin1010@gmail.com>
2021-04-16 18:33:29 -0400

    removed unneeded directories from branch

--- server/src/routes/contact.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import * as contact from "./../controllers/contact";
-
-const router = express.Router();
-
-router.post("/message", contact.createMessage);
-
-export default router;
--- server/src/utils/sendRes.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from "express";
-
-const sendRes = (res: Response, code: number, message: string) => {
-  return res.status(code).json({ message: message });
-};
-
-export default sendRes;
